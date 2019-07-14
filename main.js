470662fbdffb65bb914637c6a1da9fbe19b10eb7
threey-y <52376594+threey-y@users.noreply.github.com>
2019-07-14 17:20:51 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var user = {
+            name ="John",
+            surname="Mike"
+        };
+        user.name="Peter";
+        delete user.name;
+        
+var fruit = {
+      apple: 20,
+      pear: 20,
+      peach: 10
+      };
+console.log(fruit.apple+fruit.pear+fruit.peach);
